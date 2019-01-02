a96696030a780ab90e06e17bc1155ac36bbd3473
Dave Longbottom <davelongbottom@gmail.com>
2019-01-02 12:40:58 +1030

    basic prop passing

--- src/App.js
+++ src/App.js
@@ -2,24 +2,27 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Family extends Component {
+  render() {
+    return (
+        <Person name={this.props.name} />
+      )
+  }
+}
+
+class Person extends Component {
+  render() {
+    return (
+        <h1>hello {this.props.name}</h1>
+      )
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+         <Family name="george" />
       </div>
     );
   }
